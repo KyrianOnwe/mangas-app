0dc92560d97fe48a95c93bfda8e8890cc6b06282
Kyrian <dr.kyrian.onwe@gmail.com>
2022-12-19 16:47:40 -0600

    Setup proxy

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,20 @@
-Rails.application.routes.draw do
-  resources :mangas
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# Rails.application.routes.draw do
+#   resources :mangas
+#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "mangas#index"
+#   # Defines the root path route ("/")
+#   root "mangas#index"
+# end
+
+# config/routes.rb
+Rails.application.routes.draw do
+  namespace :api do
+    resources :recipes, only: [:index, :create]
+    post "/signup", to: "users#create"
+    get "/me", to: "users#show"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
+  end
+
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
